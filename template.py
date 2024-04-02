cdc61f94ac5f6481f158633923bf966fa4b2b56b
Isines1611 <isines16@gmail.com>
2024-04-02 17:09:00 +0200

    Done Q1

---
+++ template.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import sys 
+from template_utils import *
+
+sys.setrecursionlimit(6000)
+
+# Undirected graph
+# Task 1: Average degree, number of bridges, number of local bridges
+def Q1(dataframe):
+
+    total_degree = 0
+    num_nodes = len(set(dataframe['Src']) | set(dataframe['Dst']))
+
+    for node in set(dataframe['Src']) | set(dataframe['Dst']):
+        total_degree += node_degree(dataframe, node)
+
+    # Compute the average degree
+    average_degree = total_degree / num_nodes if num_nodes > 0 else 0
+
+    plot_degree_distribution(dataframe)
+
+    bridges = get_total_bridges(dataframe)
+    local_bridges = get_total_local_bridges(dataframe)
+
+    return [average_degree, bridges, local_bridges] # [average degree, nb bridges, nb local bridges]
+
+# Undirected graph
+# Task 2: Average similarity score between neighbors
+def Q2(dataframe):
+    # Your code here
+    return 0.0 # the average similarity score between neighbors
+
+# Directed graph
+# Task 3: PageRank
+def Q3(dataframe):
+    # Your code here
+    return [0, 0.0] # the id of the node with the highest pagerank score, the associated pagerank value.
+    # Note that we consider that we reached convergence when the sum of the updates on all nodes after one iteration of PageRank is smaller than 10^(-6)
+
+# Undirected graph
+# Task 4: Small-world phenomenon
+def Q4(dataframe):
+    # Your code here
+    return [0, 0, 0, 0, 0] # at index 0 the number of shortest paths of lenght 0, at index 1 the number of shortest paths of length 1, ...
+    # Note that we will ignore the value at index 0 as it can be set to 0 or the number of nodes in the graph
+
+# Undirected graph
+# Task 5: Betweenness centrality
+def Q5(dataframe):
+    # Your code here
+    return [0, 0.0] # the id of the node with the highest betweenness centrality, the associated betweenness centrality value.
+
+# you can write additionnal functions that can be used in Q1-Q5 functions in the file "template_utils.py", a specific place is available to copy them at the end of the Inginious task.
+
+#df = pd.read_csv('powergrid.csv')
+df = pd.read_csv('test.csv')
+print("Q1", Q1(df))
+print("Q2", Q2(df))
+print("Q3", Q3(df))
+print("Q4", Q4(df))
+print("Q5", Q5(df))
---
+++ template_utils.py
@@ -0,0 +1,114 @@
+# If needed, write here your additional fuctions/classes with their signature and use them in the exercices:
+# a specific place is available to copy them at the end of the Inginious task.
+
+# First, import the libraries needed for your helper functions
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_all_nodes(df):
+    nodes = []
+    for index, row in df.iterrows():
+        src = row['Src']
+        dst = row['Dst']
+
+        if src not in nodes:
+            nodes.append(src)
+        if dst not in nodes:
+            nodes.append(dst)
+
+    return nodes
+
+
+# Q1 Helpers
+
+def node_degree(df, node):
+    src_degree = len(df[df['Src'] == node])
+    dest_degree = len(df[df['Dst'] == node])
+
+    return src_degree + dest_degree
+
+def common_neighbors(df, n1, n2):
+    neighbors_node1 = set(df[df['Src'] == n1]['Dst']) | set(df[df['Dst'] == n1]['Src'])
+    neighbors_node2 = set(df[df['Src'] == n2]['Dst']) | set(df[df['Dst'] == n2]['Src'])
+
+    common_neighbors = neighbors_node1 & neighbors_node2
+    count = len(common_neighbors)
+
+    return count
+
+
+# Q1
+
+def get_total_local_bridges(df):
+    total = 0
+
+    for index, row in df.iterrows():
+        src = row['Src']
+        dst = row['Dst']
+
+        total_both = common_neighbors(df, src, dst)
+        total_neighbor = node_degree(df, src) + node_degree(df, dst) - 2
+
+        print("\nFor",src,"to",dst,"\nBoth:",total_both,"total:",total_both,"no=", (total_both / total_neighbor),"\n")
+
+        if(total_both / total_neighbor) == 0:
+            total += 1
+
+    return total
+
+def get_total_bridges(df): # pour le coup c'est pas moi qui l'ai fait
+    graph = {}  # create an adjacency list representation of the graph
+    for index, row in df.iterrows():
+        src = row['Src']
+        dest = row['Dst']
+
+        if src in graph:
+            graph[src].append(dest)
+        else:
+            graph[src] = [dest]
+
+        if dest in graph:
+            graph[dest].append(src)
+        else:
+            graph[dest] = [src]
+
+    num_bridges = 0
+    visited = set()
+    parent = {}
+    low = {}
+    disc = {}
+
+    def dfs(node):
+        nonlocal num_bridges
+        visited.add(node)
+        disc[node] = low[node] = len(visited)
+        for neighbor in graph.get(node, []):
+            if neighbor not in visited:
+                parent[neighbor] = node
+                dfs(neighbor)
+                low[node] = min(low[node], low[neighbor])
+                if low[neighbor] > disc[node]:
+                    num_bridges += 1
+            elif neighbor != parent.get(node, None):
+                low[node] = min(low[node], disc[neighbor])
+
+    for node in graph:
+        if node not in visited:
+            dfs(node)
+
+    return num_bridges
+
+def plot_degree_distribution(df): # Histogram
+    all_nodes = pd.concat([df['Src'], df['Dst']])
+    
+    node_counts = all_nodes.value_counts()
+
+    # Plot histogram of degree distribution
+    plt.hist(node_counts, bins=range(1, max(node_counts)+2), align='left', edgecolor='black')
+    plt.xlabel('Degree')
+    plt.ylabel('Number of Nodes')
+    plt.title('Degree Distribution')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
